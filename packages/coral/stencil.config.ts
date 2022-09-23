1505edb4fabed4cf047459896c740d2b23a39ccb
Sebastiandg7 <13395979+sebastiandg7@users.noreply.github.com>
2022-09-23 10:17:14 -0500

    first commit

---
+++ packages/coral/stencil.config.ts
@@ -0,0 +1,32 @@
+import { Config } from '@stencil/core';
+
+import { sass } from '@stencil/sass';
+
+import { reactOutputTarget } from '@stencil/react-output-target';
+
+export const config: Config = {
+  namespace: 'coral',
+  taskQueue: 'async',
+  plugins: [sass()],
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+    },
+    {
+      type: 'dist-custom-elements',
+    },
+    {
+      type: 'docs-readme',
+    },
+    {
+      type: 'www',
+      serviceWorker: null, // disable service workers
+    },
+    reactOutputTarget({
+      componentCorePackage: '@divetool/coral',
+      proxiesFile: '../../../packages/coral-react/src/generated/components.ts',
+      includeDefineCustomElements: true,
+    }),
+  ],
+};
