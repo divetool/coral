bc17cc3b871a04abc28a45311c0a5f88c6342d37
Sebastiandg7 <13395979+sebastiandg7@users.noreply.github.com>
2022-09-26 23:45:38 -0500

    feat(storybook): setup

---
+++ packages/coral/src/components/crl-button/crl-button.stories.tsx
@@ -0,0 +1,12 @@
+import { h } from '@stencil/core';
+import { CrlButton } from '@divetool/coral/crl-button';
+
+export default {
+  title: 'CrlButton',
+  component: CrlButton,
+};
+
+const Template = (args) => <crl-button {...args}></crl-button>;
+
+export const Primary = Template.bind({});
+Primary.args = { first: 'Hello', last: 'World' };
---
+++ packages/coral/src/components/crl-button/crl-button.tsx
@@ -0,0 +1,35 @@
+import { Component, h, Prop } from '@stencil/core';
+
+@Component({
+  tag: 'crl-button',
+  styleUrl: 'crl-button.scss',
+  shadow: true,
+})
+export class CrlButton {
+  /**
+   * The first name
+   */
+  @Prop() first: string;
+
+  /**
+   * The middle name
+   */
+  @Prop() middle: string;
+
+  /**
+   * The last name
+   */
+  @Prop() last: string;
+
+  private getText(): string {
+    return (
+      (this.first || '') +
+      (this.middle ? ` ${this.middle}` : '') +
+      (this.last ? ` ${this.last}` : '')
+    );
+  }
+
+  render() {
+    return <div>Hello, World! I'm {this.getText()}</div>;
+  }
+}
