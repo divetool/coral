9922e992c2a945dafe4d10f45992ea57832ba852
Andres <andresvd21@gmail.com>
2022-11-05 16:46:08 -0500

    feat(coral-tokens): adding shades transform to custom transforms index

---
+++ packages/coral-tokens/src/extensions/transforms/index.ts
@@ -0,0 +1,12 @@
+import {
+  CustomTransformsBuilder,
+} from '@nxkit/style-dictionary';
+import { ShadesTransform } from './shades-transform/transform';
+
+const customTransformsBuilder: CustomTransformsBuilder = () => {
+  return {
+    [ShadesTransform.name]: ShadesTransform.transform
+  };
+};
+
+export default customTransformsBuilder;
