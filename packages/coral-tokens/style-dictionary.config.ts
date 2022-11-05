de25f409ef9b00797d8aff752d19d1d5ae4f0620
Andres <andresvd21@gmail.com>
2022-11-05 16:47:53 -0500

    feat(coral-tokens): generation coral-tokens package

---
+++ packages/coral-tokens/style-dictionary.config.ts
@@ -0,0 +1,20 @@
+import { Config } from 'style-dictionary';
+
+const config: Config = {
+  source: ['src/tokens/**/*.json'],
+  platforms: {
+    scss: {
+      transformGroup: 'scss',
+      buildPath: 'scss/',
+      transforms: [`attribute/cti`, `name/cti/kebab`, `shades-transform`, `color/hex`],
+      files: [
+        {
+          destination: '_variables.scss',
+          format: 'scss/variables',
+        },
+      ],
+    }
+  },
+};
+
+export default config;
